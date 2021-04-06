d149f8d3cc3ba3b7b43dc2f8b7eccf637b02aeb6
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2021-04-06 14:31:10 +0800

    開始前端：1. chakraUI + nextjs

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return (
+        <Wrapper>
+            <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            color="teal"
+                            isLoading={isSubmitting}
+                        >
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Register;
